8ba41c7485087f7a312a71d1eafc44ab0070b0c9
ezen <ezen@DESKTOP-O03MT84>
2022-02-04 14:28:39 +0900

    0203

--- src/com/urdomain/lecture/ch02/Hello.java
+++ src/com/urdomain/lecture/ch02/Hello.java
@@ -4,7 +4,14 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Hell World!");
-		System.out.println("Welcome to Hell World!");
+	
+		int a = 1;
+		String str;
+		int b = 2;
+		int c = 0;
+		c = a + b;
+	
+		System.out.println(c);
 	}
 
-}
+}	
---
+++ src/com/urdomain/lecture/ch02/Variable.java
@@ -0,0 +1,25 @@
+package com.urdomain.lecture.ch02;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		//변수 명명법
+		//첫 글자 소문자 그리고 다음 문자열부터는 대문자
+		//모든 변수명은 붙여서 사용
+		//대소문자 구분함
+		
+		int roomNumber = 1;
+		int roomnumber = 2;
+		
+		System.out.println(roomNumber);
+		System.out.println(roomnumber);
+		
+		
+		boolean defaultNy = 1;
+		
+		
+
+	}
+
+}
